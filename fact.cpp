805a9b825fcb645b29c52b39b929cd48ba83faf1
Shani Kumar <shani.mnnit18@gmail.com>
2022-06-12 11:40:35 +0530

    factorial

---
+++ fact.cpp
@@ -0,0 +1,19 @@
+#include
+
+int fact(int n){
+	 if(n<=1)
+	 	return 1;
+	 return n*func(n-1);
+}
+
+
+//memoization
+// no benefits with memoization because no repeatition of problems 
+void main(){
+	int n
+	cin >> n
+	cout << fact(n)<<"\n"
+}
+
+
+//   bulldog 
